8db1319ccb85f5464329cdfcab07726435a4da69
Jamiel <20553479+jamielsharief@users.noreply.github.com>
2020-06-13 20:48:54 +0200

    Refactored to use connection::transaction

--- src/Console/Command/DbSchemaTrait.php
+++ src/Console/Command/DbSchemaTrait.php
@@ -97,24 +97,23 @@ trait DbSchemaTrait
     */
     protected function executeStatements(array $statements, Connection $connection) : int
     {
-        $connection->begin();
-        $connection->disableForeignKeyConstraints();
-        
+        $connection->transaction(function ($connection) use ($statements) {
+            $this->processStatements($connection, $statements);
+        }, true);
+        return count($statements);
+    }
+
+    protected function processStatements(Connection $connection, array $statements) : void
+    {
         foreach ($statements  as $statement) {
             try {
                 $connection->execute($statement);
             } catch (DatasourceException $ex) {
-                $connection->rollback();
                 $this->io->status('error', str_replace("\n", '', $statement));
                 $this->throwError('Executing query failed', $ex->getMessage());
             }
             $this->io->status('ok', str_replace("\n", '', $statement));
         }
-       
-        $connection->commit();
-        $connection->enableForeignKeyConstraints();
-
-        return count($statements);
     }
 
     abstract public function throwError(string $title, string $message = null) : void;
