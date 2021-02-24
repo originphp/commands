d08d0b8cdd6bdfe96c53a8c246eda2df8ad23d37
Jamiel Sharief <20553479+jamielsharief@users.noreply.github.com>
2021-02-24 11:37:49 +0100

    Added copyright header

--- tests/Task/DummyTask.php
+++ tests/Task/DummyTask.php
@@ -1,5 +1,17 @@
 <?php
-declare(strict_types = 1);
+/**
+ * OriginPHP Framework
+ * Copyright 2018 - 2021 Jamiel Sharief.
+ *
+ * Licensed under The MIT License
+ * The above copyright notice and this permission notice shall be included in all copies or substantial
+ * portions of the Software.
+ *
+ * @copyright    Copyright (c) Jamiel Sharief
+ * @link         https://www.originphp.com
+ * @license      https://opensource.org/licenses/mit-license.php MIT License
+ */
+declare(strict_types=1);
 namespace Commands\Test\Task;
 
 use Origin\Schedule\Schedule;
