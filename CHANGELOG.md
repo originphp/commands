5c441789f2cb92bf8fc759ba1055633cec48020c
Jamiel Sharief <20553479+jamielsharief@users.noreply.github.com>
2021-02-24 10:54:07 +0100

    Added schedule:run

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,5 +1,11 @@
 # Changelog
 
+## [3.3.0] 2021-02-24
+
+### Added
+
+- Added schedule:run
+
 ## [3.2.4] 2021-01-14
 
 ### Changed
@@ -78,27 +84,32 @@ Skipped this version
 ## [1.4.1] - 2020-06-17
 
 ### Fixed
+
 - Fixed db:test:prepare not removing sqlite database
 
 ## [1.4.0] - 2020-06-14
 
 ### Added
+
 - Added support for SQlite
 - Added cache clearing after database activities such as migrations
 
 ## [1.3.0] - 2020-05-10
 
 ### Added
+
 - Added check for App.schemaFormat to work with version 2.6
 
 ## [1.2.0] - 2019-11-23
 
 ### Added
+
 - Added mailbox:download command
 
 ## [1.1.0] - 2019-11-06
 
 ### Added
+
 - Added cache:clear command
 
 ## [1.0.0] - 2019-10-18
---
+++ src/Console/Command/ScheduleRunCommand.php
@@ -0,0 +1,58 @@
+<?php
+/**
+ * OriginPHP Framework
+ * Copyright 2018 - 2021 Jamiel Sharief.
+ *
+ * Licensed under The MIT License
+ * The above copyright notice and this permission notice shall be included in all copies or substantial
+ * portions of the Software.
+ *
+ * @copyright    Copyright (c) Jamiel Sharief
+ * @link         https://www.originphp.com
+ * @license      https://opensource.org/licenses/mit-license.php MIT License
+ */
+declare(strict_types = 1);
+namespace Commands\Console\Command;
+
+use Exception;
+use Origin\Schedule\Schedule;
+use Origin\Console\Command\Command;
+
+class ScheduleRunCommand extends Command
+{
+    protected $name = 'schedule:run';
+    protected $description = 'Runs the scheduled tasks';
+
+    protected function initialize(): void
+    {
+        $this->addOption('directory', [
+            'description' => 'The directory where the tasks files are'
+        ]);
+
+        $this->addOption('id', [
+            'description' => 'A specific event ID that should be run'
+        ]);
+    }
+    
+    /**
+     * @return void
+     */
+    protected function execute(): void
+    {
+        $path = $this->options('directory') ?: Schedule::config('path');
+
+        if (is_null($path)) {
+            $path = (defined('ROOT') ? ROOT : getcwd()) . '/app/Task';
+        }
+
+        if (! is_dir($path)) {
+            $this->throwError('Directory does not exist');
+        }
+  
+        try {
+            Schedule::run($path, $this->options('id'));
+        } catch (Exception $exception) {
+            $this->throwError($exception->getMessage());
+        }
+    }
+}
